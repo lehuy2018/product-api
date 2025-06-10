c936ea9f6397f1ef2468fb2fb4cdba03fb03bd23
Huy <huylv@greenglobal.vn>
2025-06-10 08:30:57 +0700

    feat(DDD) : remove lib

--- src/main/java/com/example/future/infrastructure/repository/JpaProductJpa.java
+++ src/main/java/com/example/future/infrastructure/repository/JpaProductJpa.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.future.domain.product.model.Product;
 
-@Repository
 public interface JpaProductJpa extends JpaRepository<Product, Long> {
 	List<Product> findByNameContainingIgnoreCase(String name);
 
